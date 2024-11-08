044c87641103f86015bcb29d1fa97e2af08d3a05
VuongMinhTuan04 <vuongtuan1357@gmail.com>
2024-11-08 11:12:19 +0700

    Create Project

---
+++ Backend/src/test/java/com/src/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.src.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
